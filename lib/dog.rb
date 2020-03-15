597fac48c5e7d06486ee390ecfc229744920f12f
Charles Adkins <cpeadkins@gmail.com>
2020-03-15 03:45:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,13 +3,6 @@ class Dog
   def initialize(name,breed="Mutt")
     @name = name
     @breed = breed
-   
-    # if breed.length > 0
-    #   @breed = breed
-    # elsif
-    #   @breed = "Mutt"
-    # end
-  
   end
 
 end
